908aed506b829843622cd2d83fd501787d68ee11
kankittikan <nongkan195@gmail.com>
2023-10-22 13:12:36 +0700

    init

---
+++ src/test/java/ku/cs/palmoilmnger/PalmoilMngerApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.palmoilmnger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalmoilMngerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
