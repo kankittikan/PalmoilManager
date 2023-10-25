a8fb8ac344709732f1f7739f804a82e2098d0932
kankittikan <nongkan195@gmail.com>
2023-10-25 13:42:14 +0700

    add storage static location

--- src/main/java/ku/cs/palmoilmnger/config/SecurityConfig.java
+++ src/main/java/ku/cs/palmoilmnger/config/SecurityConfig.java
@@ -1,13 +1,11 @@
 package ku.cs.palmoilmnger.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 
 @Configuration
 @EnableWebSecurity
