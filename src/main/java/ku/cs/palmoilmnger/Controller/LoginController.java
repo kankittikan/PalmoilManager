fbb5c628c07c7aa9f97f9946311581c79dcc2541
Siwakorn Pasawang <pasawang.sivakorn@gmail.com>
2023-10-25 17:11:47 +0700

    Add Login and Signup backend

--- src/main/java/ku/cs/palmoilmnger/Controller/LoginController.java
+++ src/main/java/ku/cs/palmoilmnger/Controller/LoginController.java
@@ -1,7 +1,12 @@
 package ku.cs.palmoilmnger.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
@@ -9,6 +14,15 @@ public class LoginController {
     public String getHomePage(Model model) {
         //model.addAttribute("greeting", "Sawaddee");
         return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication){
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/?logout";
     }
 
 }
