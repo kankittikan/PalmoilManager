46497159780af5c122cb467744a48f02aebcdf49
Siwakorn Pasawang <pasawang.sivakorn@gmail.com>
2023-10-27 15:19:42 +0700

    create basic method in description and work type services file

--- src/main/java/ku/cs/palmoilmnger/service/WorkTypeService.java
+++ src/main/java/ku/cs/palmoilmnger/service/WorkTypeService.java
@@ -1,4 +1,18 @@
 package ku.cs.palmoilmnger.service;
 
+import ku.cs.palmoilmnger.entity.WorkType;
+import ku.cs.palmoilmnger.repository.WorkTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class WorkTypeService {
+    @Autowired
+    WorkTypeRepository workTypeRepository;
+
+    public void createWorkType(WorkType workType){
+        workTypeRepository.save(workType);
+    }
+
+    public WorkType getWorkType(String name){
+        return workTypeRepository.findByName(name);
+    }
 }
