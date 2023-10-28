98e2fa8c96c62e2e29c8d7d5c3316008f10d86a1
Siwakorn Pasawang <pasawang.sivakorn@gmail.com>
2023-10-28 10:08:20 +0700

    add goto page

---
+++ src/main/java/ku/cs/palmoilmnger/controller/AdminCreatePalmPlotController.java
@@ -0,0 +1,22 @@
+package ku.cs.palmoilmnger.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminCreatePalmPlotController {
+
+    @GetMapping("/managePalmPlot/create")
+    public String getCreatePalmPage(Model model){
+        return "createPalmPlot";
+    }
+
+    @PostMapping("/managePalmPlot/create")
+    public String createPalmHandler(Model model){
+
+    }
+}
