a56d78609a4cddd6a13735f790b1cb93fc9c62b5
Peerasit <peerasitearth@outlook.com>
2023-10-28 12:53:28 +0700

    UI image transaction complete

--- src/main/java/ku/cs/palmoilmnger/controller/RoundAddPalmTransactionController.java
+++ src/main/java/ku/cs/palmoilmnger/controller/RoundAddPalmTransactionController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/manager")
 public class RoundAddPalmTransactionController {
 
     @RequestMapping("/menu/round/manageRound/palm/create")
