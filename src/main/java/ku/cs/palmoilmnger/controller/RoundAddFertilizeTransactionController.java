5ce11adc958690773d75ab97e559b99efeffd3ff
Peerasit <peerasitearth@outlook.com>
2023-10-28 13:19:10 +0700

    UI fertilize add transaction complete

---
+++ src/main/java/ku/cs/palmoilmnger/controller/RoundAddFertilizeTransactionController.java
@@ -0,0 +1,21 @@
+package ku.cs.palmoilmnger.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/manager")
+public class RoundAddFertilizeTransactionController {
+
+    @RequestMapping("/menu/round/manageRound/fertilize/create")
+    public String getFertilizeCreateTransactionPage(Model model) {
+        model.addAttribute("username", "ชื่อผู้ใช้");
+
+        model.addAttribute("plotName", "พลุดินนา");
+        model.addAttribute("year", "2023");
+        model.addAttribute("month", "พฤษภาคม");
+        model.addAttribute("round", "รอบ "+"1");
+        return "fertilizeCreateTransaction";
+    }
+}
