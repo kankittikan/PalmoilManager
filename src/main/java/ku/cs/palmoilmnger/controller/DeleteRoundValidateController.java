e6e9c2765dd3fe56e9f1ac300f195c3d8b785551
Peerasit <peerasitearth@outlook.com>
2023-10-28 19:54:49 +0700

    update botton manage round and delete round

--- src/main/java/ku/cs/palmoilmnger/controller/DeleteRoundValidateController.java
+++ src/main/java/ku/cs/palmoilmnger/controller/DeleteRoundValidateController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/manager")
 public class DeleteRoundValidateController {
 
     @RequestMapping("/menu/round/ConfirmDeleteRound")
