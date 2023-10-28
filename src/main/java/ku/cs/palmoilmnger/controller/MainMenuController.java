ad857efba30df8991177eb9791de7a61e171b10d
Siwakorn Pasawang <pasawang.sivakorn@gmail.com>
2023-10-28 14:04:54 +0700

    Make go to summary

--- src/main/java/ku/cs/palmoilmnger/controller/MainMenuController.java
+++ src/main/java/ku/cs/palmoilmnger/controller/MainMenuController.java
@@ -4,6 +4,7 @@ import ku.cs.palmoilmnger.service.PlantationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +16,17 @@ public class MainMenuController {
     public String getMenuPage(Model model) {
         model.addAttribute("plots", plantationService.getAllPlantation());
         return "menu";
+    }
+
+    @GetMapping("/menu/quarterSummary")
+    public String getQuarterSummaryPage(Model model) {
+        model.addAttribute("username", "ผู้ใช้");
+        return "quarterSummary";
+    }
+
+    @GetMapping("/menu/yearSummary")
+    public String getYearSummaryPage(Model model) {
+        model.addAttribute("username", "ผู้ใช้");
+        return "yearSummary";
     }
 }
